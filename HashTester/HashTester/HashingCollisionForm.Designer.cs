b7840c764abe6f4e25bf53ceca252f585231e8a6
Kokorotko <prdak05@seznam.cz>
2024-12-25 22:13:27 +0100

    HashingCollisionForm Fucking Done
    This took fucking 4 hours of pure pain and MultiThreading

--- HashTester/HashTester/HashingCollisionForm.Designer.cs
+++ HashTester/HashTester/HashingCollisionForm.Designer.cs
@@ -38,7 +38,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.labelTimer = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // hashSelector
@@ -47,12 +51,10 @@
             this.hashSelector.Cursor = System.Windows.Forms.Cursors.Default;
             this.hashSelector.FormattingEnabled = true;
             this.hashSelector.Items.AddRange(new object[] {
-            "MD5",
-            "SHA1",
-            "SHA256",
-            "SHA512",
+            "CRC32",
             "RipeMD-160",
-            "CRC32"});
+            "MD5",
+            "SHA1"});
             this.hashSelector.Location = new System.Drawing.Point(12, 41);
             this.hashSelector.Name = "hashSelector";
             this.hashSelector.Size = new System.Drawing.Size(121, 21);
@@ -108,6 +110,11 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(12, 84);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 15;
@@ -140,11 +147,49 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Lenght of the random text";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(139, 84);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 19;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Number of attempts: ";
+            // 
             // HashingCollisionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.labelTimer);
             this.Controls.Add(this.label1);
@@ -158,6 +203,7 @@
             this.Name = "HashingCollisionForm";
             this.Text = "HashingCollisionForm";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +221,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelTimer;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- HashTester/HashTester/HashingCollisionForm.cs
+++ HashTester/HashTester/HashingCollisionForm.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer; // Using Windows Forms Timer
 
 namespace HashTester
 {
+    public delegate void UpdateAttemptsLabelDelegate(long attempts);
+
     public partial class HashingCollisionForm : Form
     {
         public HashingCollisionForm()
         {
             InitializeComponent();
         }
+
         Hasher hasher = new Hasher();
-        bool stopHashing = false;
+        Hasher.HashingAlgorithm algorithm = Hasher.HashingAlgorithm.CRC32;
+        Thread collisionThread;
+        volatile bool stopHashing = false; // Volatile for thread safety
+        long maxAttempts = 0;
+        long attempts = 0;
+        int timeToCalculate = 0;
+
+        private Timer timer; // Declare a Timer object for updating timer label.
 
         private void buttonClearListBox_Click(object sender, EventArgs e)
         {
@@ -31,129 +40,115 @@ namespace HashTester
 
         private void buttonGenerateCollision_Click(object sender, EventArgs e)
         {
-            bool foundCollision = false;
+            TurnOffUI();
+            stopHashing = false; 
+            maxAttempts = (long)numericUpDown1.Value;
+            int length = (int)numericUpDown2.Value;
+            //Timer
+            timeToCalculate = 0;
+            timer = new Timer();
+            timer.Interval = 100;
+            timer.Tick += (s, args) => UpdateTimerLabel();
+            timer.Start();
+
+            switch (hashSelector.SelectedIndex)
+            {
+                case 0: algorithm = Hasher.HashingAlgorithm.CRC32; break;
+                case 1: algorithm = Hasher.HashingAlgorithm.RIPEMD160; break;
+                case 2: algorithm = Hasher.HashingAlgorithm.MD5; break;
+                case 3: algorithm = Hasher.HashingAlgorithm.SHA1; break;
+                default: algorithm = Hasher.HashingAlgorithm.CRC32; break;
+            }
+
+            collisionThread = new Thread(() => CollisionThread(algorithm, maxAttempts, length));
+            collisionThread.Start();
+        }
+
+        private void CollisionThread(Hasher.HashingAlgorithm algorithm, long maxAttempts, int length)
+        {
+            bool foundCollision = false, attemptsRanOut = false;
             string textCollision01 = "", textCollision02 = "";
-            long attempts = 0;
-            Timer timeToFind = new Timer();
+            attempts = 0; // Reset attempts
 
-            if (hashSelector.SelectedIndex != 5)
-            {
-                CollisionChecker collisionChecker = new CollisionChecker();
-                if (collisionChecker.ShowDialog() == DialogResult.OK)
-                {
-                    switch (hashSelector.SelectedIndex)
-                    {
-                        case 0: // MD5
-                            {
-                                foundCollision = GenerateCollision(Hasher.HashingAlgorithm.MD5, out textCollision01, out textCollision02, out attempts, out timeToFind);
-                                break;
-                            }
-                        case 1: // SHA1
-                            {
-                                foundCollision = GenerateCollision(Hasher.HashingAlgorithm.SHA1, out textCollision01, out textCollision02, out attempts, out timeToFind);
-                                break;
-                            }
-                        case 2: // SHA256
-                            {
-                                foundCollision = GenerateCollision(Hasher.HashingAlgorithm.SHA256, out textCollision01, out textCollision02, out attempts, out timeToFind);
-                                break;
-                            }
-                        case 3: // SHA512
-                            {
-                                foundCollision = GenerateCollision(Hasher.HashingAlgorithm.SHA512, out textCollision01, out textCollision02, out attempts, out timeToFind);
-                                break;
-                            }
-                        case 4: // RipeMD-160
-                            {
-                                foundCollision = GenerateCollision(Hasher.HashingAlgorithm.RIPEMD160, out textCollision01, out textCollision02, out attempts, out timeToFind);
-                                break;
-                            }
-                        default:
-                            {
-                                MessageBox.Show("Invalid hash algorithm selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                return;
-                            }
-                    }
-                }
-            }
-            else // CRC32
-            {
-                foundCollision = GenerateCollision(Hasher.HashingAlgorithm.CRC32, out textCollision01, out textCollision02, out attempts, out timeToFind);
-            }
+            bool useAttempts = false;
+            if (maxAttempts > 0) useAttempts = true;
+            foundCollision = GenerateCollision(algorithm, length, maxAttempts, useAttempts, out textCollision01, out textCollision02, out attemptsRanOut);
 
+            // Stop the timer when the collision process finishes
+            timer.Stop();
+
+            // Show results
             if (foundCollision)
             {
-                if (!string.IsNullOrEmpty(textCollision01) && !string.IsNullOrEmpty(textCollision02))
-                {
-                    MessageBox.Show("Collision found!\n" +
-                                    "Collision 1: " + textCollision01 + Environment.NewLine +
-                                    "Collision 2: " + textCollision02 + Environment.NewLine +
-                                    "Attempts: " + attempts.ToString() + Environment.NewLine +
-                                    "Time Taken: " + timeToFind.Interval.ToString() + " ms",
-                                    "Collision Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("An error occurred while finding the collision.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+                string message = "Collision found!" +
+                                 "\nCollision 1: " + textCollision01 +
+                                 "\nCollision 2: " + textCollision02 +
+                                 "\nAttempts: " + attempts + 
+                                 "\nTime to find: " + labelTimer.Text;
+                Invoke((Action)(() => MessageBox.Show(message, "Collision Found", MessageBoxButtons.OK, MessageBoxIcon.Information)));
+            }
+            else if (stopHashing)
+            {
+                Invoke((Action)(() => MessageBox.Show("The process has been abandoned.", "Abandoned", MessageBoxButtons.OK, MessageBoxIcon.Warning)));
+            }
+            else if (attemptsRanOut)
+            {
+                Invoke((Action)(() => MessageBox.Show("Could not find a collision under the given attempts.", "Abandoned", MessageBoxButtons.OK, MessageBoxIcon.Warning)));
             }
             else
             {
-                MessageBox.Show("The process has been abandoned.", "Abandoned", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Invoke((Action)(() => MessageBox.Show("Could not find collision.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)));
             }
+
+            Invoke((Action)TurnOnUI);
         }
 
-
-
-        private bool GenerateCollision(Hasher.HashingAlgorithm algorithm, out string originalTextCollision01, out string originalTextCollision02, out long attemps, out Timer timeToFind)
+        private bool GenerateCollision(Hasher.HashingAlgorithm algorithm, int length, long maxAttempts, bool useAttempts, out string collision1, out string collision2, out bool attemptsRanOut)
         {
+            collision1 = "";
+            collision2 = "";
+            attemptsRanOut = false;
             bool foundCollision = false;
-            List<string> collisionList = new List<string>();
-            List<string> collisionListOriginalText = new List<string>();
-            originalTextCollision01 = "";
-            originalTextCollision02 = "";
-            attemps = 0;
-            Random random = new Random();
-            bool useAttempts = true;
-            bool attemptsRanOut = false;
-            decimal maxAttempts = numericUpDown1.Value;
-            if (maxAttempts == 0) useAttempts = false;
-            timeToFind = new Timer();
-            timeToFind.Start();
+            var random = new Random();
+            var collisionList = new List<string>();
+            var collisionListOriginalText = new List<string>();
+
             while (!foundCollision && !stopHashing && !attemptsRanOut)
             {
-                labelTimer.Text = "Timer: " + timeToFind.Interval.ToString() + " ms";
-                attemps++;
-                // Generate a random string
-                string randomText = GenerateRandomString(random, 10);
-                string hashedValue = hasher.Hash(randomText, algorithm);
+                // Increment attempts atomically
+                Interlocked.Increment(ref attempts);
 
+                // Update attempts label
+                Invoke((Action)UpdateAttemptsLabel);
+
+                // Generate a random string
+                string randomText = GenerateRandomString(random, length);
+                string hashedValue = hasher.Hash(randomText, algorithm);
+                Console.WriteLine("Random Text: " + hashedValue);
                 // Check for collision
                 if (collisionList.Contains(hashedValue))
                 {
                     int collisionIndex = collisionList.IndexOf(hashedValue);
-                    originalTextCollision01 = collisionListOriginalText[collisionIndex];
-                    originalTextCollision02 = randomText;
-                    foundCollision = true;
+                    collision1 = collisionListOriginalText[collisionIndex];
+                    collision2 = randomText;
+                    if (collision1 != collision2) foundCollision = true;
+                    else
+                    {
+                        Console.WriteLine("Found duplicate: " + collision1 + " " + collision2);
+                        collisionList.RemoveAt(collisionIndex); //Removes the duplicate
+                        collisionListOriginalText.RemoveAt(collisionIndex);
+                    }
                 }
                 else
                 {
                     collisionList.Add(hashedValue);
                     collisionListOriginalText.Add(randomText);
                 }
-                //Attempts
-                if (useAttempts)
-                {
-                    if (attemps >= maxAttempts) attemptsRanOut = true;
-                }
+
+                // Check if maximum attempts are reached
+                if (useAttempts && attempts >= maxAttempts) attemptsRanOut = true;
             }
-            timeToFind.Stop();
-            if (stopHashing)
-            {
-                MessageBox.Show("The collision finding has been abandoned", "God Have Mercy", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                return false;
-            }
-            return true;
+            return foundCollision;
         }
 
         private string GenerateRandomString(Random random, int length)
@@ -165,6 +160,35 @@ namespace HashTester
         private void button1_Click(object sender, EventArgs e)
         {
             stopHashing = true;
+        }
+
+        private void TurnOffUI()
+        {
+            buttonGenerateCollision.Enabled = false;
+            buttonTakeCollisionFromTXT.Enabled = false;
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            hashSelector.Enabled = false;
+        }
+
+        private void TurnOnUI()
+        {
+            buttonGenerateCollision.Enabled = true;
+            buttonTakeCollisionFromTXT.Enabled = true;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            hashSelector.Enabled = true;
+        }
+
+        private void UpdateAttemptsLabel()
+        {
+            label3.Text = "Number of attempts: " + attempts;
+        }
+
+        private void UpdateTimerLabel()
+        {
+            labelTimer.Text = "Timer: " + (timeToCalculate / 10) + "." + (timeToCalculate % 10) + "s";
+            timeToCalculate++;
         }
     }
 }
